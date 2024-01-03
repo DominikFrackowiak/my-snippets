d7463d6b0996c94312a10e4292125adcaf75cac8
DominikFrackowiak <dominikfrac@gmail.com>
2024-01-03 13:26:09 +0100

    loading page added

---
+++ src/app/loading.tsx
@@ -0,0 +1,15 @@
+export default function Loading() {
+	return (
+		<div
+			style={{
+				height: '100vh',
+				width: '100vw',
+				display: 'flex',
+				justifyContent: 'center',
+				alignContent: 'center',
+			}}
+		>
+			<h2>Loading...</h2>
+		</div>
+	)
+}
