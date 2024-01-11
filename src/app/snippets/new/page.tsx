0e581f0314f4ad57d86e9dcb2b714f23d472f177
DominikFrackowiak <dominikfrac@gmail.com>
2024-01-11 21:53:51 +0100

    initial commit: solution to the problem found

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -3,6 +3,21 @@ import { redirect } from 'next/navigation'
 
 import { currentUser } from '@clerk/nextjs'
 
+export function Loading() {
+	return (
+		<div
+			style={{
+				height: '80vh',
+				width: '100%',
+				display: 'flex',
+				justifyContent: 'center',
+				alignItems: 'center',
+			}}
+		>
+			<h2>Loading...</h2>
+		</div>
+	)
+}
 
 export default async function SnippetCreatePage() {
 	const user = await currentUser()
@@ -27,18 +42,20 @@ export default async function SnippetCreatePage() {
 			throw new Error('User not logged in')
 		}
 
-		// Sprawdzenie, czy użytkownik istnieje w bazie danych
+		// redirect('/snippets/new?loading=true')
+
+		// Check if the user exists in the db
 		const existingUser = await db.user.findUnique({
 			where: { id: userId },
 		})
 
-		// Jeśli użytkownik nie istnieje, stwórz go
+		// If the user does not exist, create it
 		if (!existingUser) {
 			await db.user.create({
 				data: {
 					id: userId,
 					name: userName,
-     email
+					email,
 				},
 			})
 		}
@@ -77,54 +94,58 @@ export default async function SnippetCreatePage() {
 		}
 
 		// Redirect
+
 		redirect('/')
 	}
 
 	return (
-		<form action={createSnippet}>
-			<h3 className='font-bold m-3'>Create a Snippet</h3>
-			<div className='flex flex-col gap-4'>
-				<div className='flex gap-4'>
-					<label className='w-12' htmlFor='title'>
-						Title
-					</label>
-					<input
-						name='title'
-						id='title'
-						className='border rounded p-2 w-full'
-						required
-						type='text'
-					/>
-				</div>
+		<>
+			{/* {loading && <Loading />} */}
+			<form action={createSnippet}>
+				<h3 className='font-bold m-3'>Create a Snippet</h3>
+				<div className='flex flex-col gap-4'>
+					<div className='flex gap-4'>
+						<label className='w-12' htmlFor='title'>
+							Title
+						</label>
+						<input
+							name='title'
+							id='title'
+							className='border rounded p-2 w-full'
+							required
+							type='text'
+						/>
+					</div>
 
-				<div className='flex gap-4'>
-					<label className='w-12' htmlFor='code'>
-						Code
-					</label>
-					<textarea
-						name='code'
-						id='code'
-						className='border rounded p-2 w-full'
-						required
-					/>
-				</div>
+					<div className='flex gap-4'>
+						<label className='w-12' htmlFor='code'>
+							Code
+						</label>
+						<textarea
+							name='code'
+							id='code'
+							className='border rounded p-2 w-full'
+							required
+						/>
+					</div>
 
-				<div className='flex gap-4'>
-					<label className='w-12' htmlFor='tags'>
-						Tags
-					</label>
-					<input
-						name='tags'
-						id='tags'
-						className='border rounded p-2 w-full'
-						type='text'
-					/>
-				</div>
+					<div className='flex gap-4'>
+						<label className='w-12' htmlFor='tags'>
+							Tags
+						</label>
+						<input
+							name='tags'
+							id='tags'
+							className='border rounded p-2 w-full'
+							type='text'
+						/>
+					</div>
 
-				<button className='rounded p-2 bg-blue-200' type='submit'>
-					Create
-				</button>
-			</div>
-		</form>
+					<button className='rounded p-2 bg-blue-200' type='submit'>
+						Create
+					</button>
+				</div>
+			</form>
+		</>
 	)
 }
