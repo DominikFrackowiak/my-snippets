bb610f1520a3ba4e0ac96600a070ccd8a2779862
DominikFrackowiak <dominikfrac@gmail.com>
2024-01-15 19:28:11 +0100

    responsiveness issues fixed

--- src/app/snippets/page.tsx
+++ src/app/snippets/page.tsx
@@ -47,16 +47,12 @@ export default async function Home({ searchParams }: any) {
 				<div>{snippet.title}</div>
 				<div>View</div>
 			</Link>
-			<ul className='flex gap-4'>
+			<ul className='flex gap-4 w-full flex-wrap'>
 				{snippet.tags.map(tag => (
-					<Link
-						key={tag.id}
-						href={`/snippets?query=${tag.id}`}
-						className='border border-blue-600 rounded px-2 py-1 mt-2 text-blue-600'
-					>
-						<li>
+					<Link key={tag.id} href={`/snippets?query=${tag.id}`}>
+						<button className='border border-blue-600 rounded px-2 py-1 mt-2 text-blue-600'>
 							<small>{tag.id}</small>
-						</li>
+						</button>
 					</Link>
 				))}
 			</ul>
